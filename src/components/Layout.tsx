6017a3307bd7ab552b6d9a32e37fe4264ab5014d
elsa sabilah <elsabilah1@gmail.com>
2023-01-09 19:32:31 +0700

    create Modal Component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,7 @@
 import Icon from '@/components/utilities/Icon'
 import Button from '@/components/utilities/Button'
+import Modal from './utilities/Modal'
+import { useState } from 'react'
 
 interface IProps {
   children: React.ReactNode
@@ -7,18 +9,24 @@ interface IProps {
 
 function Layout(props: IProps) {
   const { children } = props
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className='max-w-[1400px] min-h-screen mx-auto flex flex-col'>
-      <header className='py-[18px] px-5 border-b border-[#E0E0E0] flex gap-[10px] items-center'>
-        <h1 className='font-bold text-lg text-[#1E1F21]'>Product Roadmap</h1>
-        <Button>
-          <Icon iconName='plus' />
-          Add New Group
-        </Button>
-      </header>
-      <main className='flex-1 px-6 pt-6'>{children}</main>
-    </div>
+    <>
+      <Modal title='Add New Group' isOpen={isOpen} setIsOpen={setIsOpen}>
+        content
+      </Modal>
+      <div className='max-w-[1400px] min-h-screen mx-auto flex flex-col'>
+        <header className='py-[18px] px-5 border-b border-[#E0E0E0] flex gap-[10px] items-center'>
+          <h1 className='font-bold text-lg text-[#1E1F21]'>Product Roadmap</h1>
+          <Button onClick={() => setIsOpen(true)}>
+            <Icon iconName='plus' />
+            Add New Group
+          </Button>
+        </header>
+        <main className='flex-1 px-6 pt-6'>{children}</main>
+      </div>
+    </>
   )
 }
 
---
+++ src/components/utilities/Modal.tsx
@@ -0,0 +1,66 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, ReactNode } from 'react'
+import Icon from './Icon'
+
+interface IProps {
+  isOpen: boolean
+  setIsOpen: (isOpen: boolean) => void
+  title: string
+  children: ReactNode
+}
+
+function Modal(props: IProps) {
+  const { isOpen, setIsOpen, title, children } = props
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        as='div'
+        className='relative z-10'
+        onClose={() => setIsOpen(false)}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-[#404040]/50' />
+        </Transition.Child>
+
+        <div className='fixed inset-0 overflow-y-auto'>
+          <div className='flex min-h-full items-center justify-center'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 scale-95'
+              enterTo='opacity-100 scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 scale-100'
+              leaveTo='opacity-0 scale-95'
+            >
+              <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-[10px] bg-white shadow transition-all'>
+                <Dialog.Title
+                  as='div'
+                  className='flex justify-between items-center p-6'
+                >
+                  <h3 className='text-lg font-bold text-[##1D1F20]'>{title}</h3>
+                  <button onClick={() => setIsOpen(false)}>
+                    <Icon iconName='close' />
+                  </button>
+                </Dialog.Title>
+
+                <div className='px-6 pb-6'>{children}</div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
+
+export default Modal
