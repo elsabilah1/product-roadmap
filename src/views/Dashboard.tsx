667337e32de18cf930aea023f0f27a6d8d236de7
elsa sabilah <elsabilah1@gmail.com>
2023-01-10 12:20:01 +0700

    api get group tasks

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,11 +1,41 @@
 import GroupTaskCard from '@/components/GroupTaskCard'
 import Layout from '@/components/Layout'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useCookies } from 'react-cookie'
+
+interface IData {
+  id: string
+  title: string
+  description: string
+}
 
 function Dashboard() {
+  const [cookies] = useCookies(['token'])
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get(`${import.meta.env.VITE_API_URL}/todos`, {
+        headers: {
+          Authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      setData(res.data)
+    }
+    getData()
+  }, [])
+
   return (
     <Layout>
-      <div className='grid grid-cols-4 gap-4'>
-        <GroupTaskCard title='Group Task 1' description='Jan - Feb' />
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {data.map((item: IData) => (
+          <GroupTaskCard
+            title={item.title}
+            description={item.description}
+            key={item.id}
+          />
+        ))}
       </div>
     </Layout>
   )
